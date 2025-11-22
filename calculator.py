1214183d0df2f9d7ea98dd1d92cbdb3c96fc60b4
ufatima-se <uuach123@gmail.com>
2025-11-22 11:51:42 +0500

    Added calculator feature by Uswa

--- calculator.py
+++ calculator.py
@@ -18,7 +18,28 @@ def calculator_AimanAsghar():
     except ValueError:
         print("\nError: Invalid input! Please enter numeric values only.")
 def guessing_game_UswaFatima():
-    print("\n[!] The Guessing Game feature is currently under construction by the teammate.")
+    import random
+def hint_by_me(guess, secret):
+    if guess > secret:
+        return "Too high!"
+    else:
+        return "Too low!"
+while True:
+    secret_number = random.randint(1, 50)
+    attempts = 0
+    print("I have selected a number between 1 and 50.")
+    while True:
+        guess = int(input("Enter your guess: "))
+        attempts += 1
+        if guess == secret_number:
+            print(f"Correct! You guessed it in {attempts} attempts.")
+            break
+        else:
+            print(hint_by_me(guess, secret_number))
+    play_again = input("Do you want to play again? (y/n): ").lower()
+    if play_again != 'y':
+        print("Thanks for playing!!!!")
+        break
 def main_menu():
     while True:
         print("\n==========================")
